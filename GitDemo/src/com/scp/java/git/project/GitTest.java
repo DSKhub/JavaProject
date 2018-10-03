620e136c7df0e4c3f1375070f21935a5d2407f41
DSKhub <dipalikokate@gmail.com>
2018-10-03 23:14:11 +0530

    first commit

---
+++ GitDemo/src/com/scp/java/git/project/GitTest.java
@@ -0,0 +1,14 @@
+package com.scp.java.git.project;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		int i=10;
+		if(i==10){
+			System.out.println("i values is :"+i);
+		}
+		else{
+			System.out.println("i values is not :"+i);
+		}
+	}
+}
